3953934c237673594b4e083239aaa3a860faf8b7
valf2k3 <valf2k3@gmail.com>
2023-03-15 17:32:48 +0300

    Памятка обновлена под законку + добавлен новый контент, вырезаны законы, читать на форуме

--- lspd/spoiler.js
+++ lspd/spoiler.js
@@ -5,14 +5,14 @@ class Spoiler{
     }
 
     add(__button_id, __element_id){
-        this.elements.push([__button_id, __element_id])
+        this.elements[__button_id] = __element_id
     }
 
     init(){
         for(let msv in this.elements){
-            e.classList.add("hidden");
-            document.getElementById(msv[0]).addEventListener('click', () => {
-                let e = document.getElementById(msv[1]);
+            document.getElementById(this.elements[msv]).classList.add("hidden");
+            document.getElementById(msv).addEventListener('click', () => {
+                let e = document.getElementById(this.elements[msv]);
                 if(e.classList.contains("hidden")){
                     e.classList.remove("hidden");
                 } else {
